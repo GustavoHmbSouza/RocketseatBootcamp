0b48494060994f7b97a2027625d1d4e69833bdad
Gustavo_Matos <gustavo.matos@smn.com.br>
2019-10-23 10:33:45 -0300

    Adicionando repositórios / Css dinâmico / Listagem dos repositórios

--- modulo5/src/pages/main/index.js
+++ modulo5/src/pages/main/index.js
@@ -1,23 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
-import { Container, Form, SubmitButton } from './styles';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Container, Form, SubmitButton, List } from './styles';
 
-export default function main() {
-    return (
-        <Container>
-            <h1>
-                <FaGithubAlt />
-                Repositorios
-            </h1>
+import api from '../../services/app';
 
-            <Form onSubmit={() => {}}>
-                <input type="text" placeholder="Adicionar repositório" />
+export default class main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newRepo: '',
+            repositories: [],
+            loading: false,
+        };
+    }
 
-                <SubmitButton disabled>
-                    <FaPlus color="#fff" size={14} />
-                </SubmitButton>
-            </Form>
-        </Container>
-    );
+    handleInputChange = e => {
+        this.setState({ newRepo: e.target.value });
+    };
+
+    handleSubmit = async e => {
+        e.preventDefault();
+
+        this.setState({ loading: true });
+
+        const { newRepo, repositories } = this.state;
+        const response = await api.get(`/repos/${newRepo}`);
+
+        const data = {
+            name: response.data.full_name,
+        };
+
+        this.setState({
+            newRepo: '',
+            repositories: [...repositories, data],
+            loading: false,
+        });
+
+        // rocketseat/unform
+        // gustavohmbsouza/rocketseatbootcamp
+    };
+
+    render() {
+        const { newRepo, repositories, loading } = this.state;
+
+        return (
+            <Container>
+                <h1>
+                    <FaGithubAlt />
+                    Repositorios
+                </h1>
+
+                <Form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Adicionar repositório"
+                        valor={newRepo}
+                        onChange={this.handleInputChange}
+                    />
+
+                    <SubmitButton loading={loading}>
+                        {loading ? (
+                            <FaSpinner color="#FFF" size={14} />
+                        ) : (
+                            <FaPlus color="#fff" size={14} />
+                        )}
+                    </SubmitButton>
+                </Form>
+                <List>
+                    {repositories.map(repository => (
+                        <li key={repository.name}>
+                            <spam>{repository.name}</spam>
+                            <a href="">Detalhes</a>
+                        </li>
+                    ))}
+                </List>
+            </Container>
+        );
+    }
 }
--- modulo5/src/pages/main/styles.js
+++ modulo5/src/pages/main/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 export const Container = styled.div`
     max-width: 700px;
@@ -12,10 +12,6 @@ export const Container = styled.div`
         font-size: 20px;
         display: flex;
         align-items: center;
-    }
-
-    svg {
-        margin-right: 10px;
     }
 `;
 
@@ -32,9 +28,20 @@ export const Form = styled.form`
     }
 `;
 
-export const SubmitButton = styled.button.attrs({
+const rotate = keyframes`
+    from {
+        transform: rotate(0deg);
+    }
+
+    to {
+        transform: rotate(360deg);
+    }
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
     type: 'submity',
-})`
+    disabled: props.loading,
+}))`
     background: #7159c1;
     border: 0;
     padding: 0 15px;
@@ -44,4 +51,52 @@ export const SubmitButton = styled.button.attrs({
     display: flex;
     justify-content: center;
     align-items: center;
+
+    &[disabled] {
+        cursor: not-allowed;
+        opacity: 0.6;
+    }
+
+    /* rodar infinitamente
+    svg {
+        animation: ${rotate} 2s infinite;
+    } */
+
+    ${props =>
+        props.loading &&
+        css`
+            svg {
+                animation: ${rotate} 2s infinite;
+            }
+        `}
+
+`;
+
+export const List = styled.div`
+    list-style: none;
+    margin-top: 30px;
+    margin-left: 10px;
+
+    li {
+        padding: 15px 0;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        /* Aplica somente nos li que tem um irmão a direita,
+        Ou seja, acaba ignorando o primeiro li. Ex:
+        li --> este será ignorado
+        li
+        li
+        li
+        */
+        & + li {
+            border-top: 1px solid #eee;
+        }
+
+        a {
+            color: #7159c1;
+            text-decoration: none;
+        }
+    }
 `;
