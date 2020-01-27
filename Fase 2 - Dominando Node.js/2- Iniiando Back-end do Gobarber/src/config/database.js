f1dceff0112ac4092646afd436889209656ff900
Gustavo Matos <gustavo.matos@smn.com.br>
2020-01-27 16:57:55 -0300

    Fase - 2 Sequelize configurado

---
+++ Fase 2 - Dominando Node.js/2- Iniiando Back-end do Gobarber/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: "postegres",
+    host: "localhost",
+    username: "postgres",
+    password: "docker",
+    database: "gobarber",
+    define: {
+        timestamps: true,
+        underscore: true,
+        underscoreAll: true,
+    },
+};
